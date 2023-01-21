ce3c32dfc0a8d3e7eda0c8c20ff4f7996a1fa795
Chris Towles <Chris.Towles@gmail.com>
2023-01-20 23:49:48 -0500

    chore: client added vue

---
+++ packages/client/src/setup/main.ts
@@ -0,0 +1,32 @@
+/* __imports__ */
+
+// import type { AppContext } from '@flashcard-dev/types'
+import { MotionPlugin } from '@vueuse/motion'
+import StarportPlugin from 'vue-starport'
+
+import type { App, Ref } from 'vue'
+import type { Router } from 'vue-router'
+
+export interface AppContext {
+  app: App
+  router: Router
+}
+
+export default function setupMain(context: AppContext) {
+  function setMaxHeight() {
+    // disable the mobile navbar scroll
+    // see https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`)
+  }
+  setMaxHeight()
+  window.addEventListener('resize', setMaxHeight)
+
+  context.app.use(MotionPlugin)
+  context.app.use(StarportPlugin({ keepAlive: true }))
+
+  // @ts-expect-error inject in runtime
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const injection_arg = context
+
+  /* __injections__ */
+}
